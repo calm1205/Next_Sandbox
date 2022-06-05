d72572f03a62bb4ab242da9d7d2076030fdfd2af
calm1205 <naginani072@gmail.com>
2022-06-05 14:24:27 +0900

    dynamic route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import Link from 'next/link';
 import React from 'react';
 
 const HomePage: React.FC = () => {
-  return <h1>welcome to next.js</h1>;
+  return (
+    <>
+      <h1>welcome to next.js</h1>
+
+      <Link href="/about">Go to /about</Link>
+      <br />
+      <Link href="/dynamic/1">Go to /dynamic/1</Link>
+    </>
+  );
 };
 
 export default HomePage;
